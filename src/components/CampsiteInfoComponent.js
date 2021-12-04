29409da2cdf0783892501edbcde5aab98aadd657
Rameen Sahoo <rameensahoo@gmail.com>
2021-12-03 23:08:18 -0800

    Redux Thunk and React-Redux-Form

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, 
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import ModalHeader from "reactstrap/lib/ModalHeader";
+import { Loading } from './LoadingComponent';
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -108,6 +109,26 @@ function RenderCampsite({campsite}) {
   }
 
   function CampsiteInfo(props) {
+    if (props.isLoading) {
+         return (
+            <div className="container">
+                 <div className="row">
+                     <Loading />
+                 </div>
+            </div>
+         );
+     }
+     if (props.errMess) {
+         return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                     </div>
+                 </div>
+             </div>
+         );
+    }
     if (props.campsite) {
       return (
         <div className="container">
